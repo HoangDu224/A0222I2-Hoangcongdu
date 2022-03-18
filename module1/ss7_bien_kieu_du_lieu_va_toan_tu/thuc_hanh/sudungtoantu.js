52d0680376b013650d08c6335304036820e7e105
Hoangdu224 <duhoang224@gmail.com>
2022-03-18 21:11:08 +0700

    baitap

---
+++ module1/ss7_bien_kieu_du_lieu_va_toan_tu/thuc_hanh/sudungtoantu.js
@@ -0,0 +1,9 @@
+
+let inputwidht;
+let inputheight;
+inputwidht = prompt("Nhap chieu dai");
+inputheight = prompt("nhap chieu rong");
+let width = parseInt(inputwidht);
+let height = parseInt(inputheight);
+let area = width*height;
+document.write("Dien tich=" +area)
