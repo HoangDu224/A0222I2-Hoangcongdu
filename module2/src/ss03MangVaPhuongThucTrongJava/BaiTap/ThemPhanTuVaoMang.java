404bdca8bacb9361ac772b8375b0c760ff2a8862
Hoangdu224 <duhoang224@gmail.com>
2022-05-23 18:55:35 +0700

    Bai Tap

--- module2/src/ss03MangVaPhuongThucTrongJava/BaiTap/ThemPhanTuVaoMang.java
+++ module2/src/ss03MangVaPhuongThucTrongJava/BaiTap/ThemPhanTuVaoMang.java
@@ -1,33 +1,44 @@
-package ss03MangVaPhuongThucTrongJava.BaiTap;
+package BaiTap.ss03;
 
 import java.util.Scanner;
 
 public class ThemPhanTuVaoMang {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int size;
-        int[] array;
-        System.out.println("Enter Size: ");
-        size = scanner.nextInt();
-        array = new int[size];
-        for (int i = 0 ; i<array.length;i++){
-            System.out.println("Input index "+i);
-            array[i]=scanner.nextInt();
+        System.out.printf("Enter size: ");
+        int size = scanner.nextInt();
+        int OldArray[]=new int[size];
+        for (int i = 0 ; i <OldArray.length;i++){
+            System.out.println("Input value index "+i);
+            OldArray[i]= scanner.nextInt();
         }
-        System.out.println("Nhap so can chen:");
+        System.out.println("Mang da nhap vao: ");
+        for (int i = 0 ; i <OldArray.length;i++){
+            System.out.print(OldArray[i]+"\t");
+        }
+        System.out.println("\nNhap gia tri muon them vao: ");
         int value = scanner.nextInt();
-        System.out.println("Nhap vi tri can chen");
-        int index = scanner.nextInt();
-        if (index<=1 || index>=array.length-1){
-            System.out.println("không chèn được phần tử vào mảng.");
-        }else {
-            array[index]=value;
-            for (int i = index+1 ;index<array.length;index++){
-            }
-        for (int i = 0 ; i<array.length;i++){
-            System.out.print(array[i]+"\t");
+        System.out.println("Nhap vi tri :");
+        int position = scanner.nextInt();
+
+        int n = OldArray.length;
+        int array[] = new int[n+1];
+        while (position < 0 || position>array.length) {
+            System.out.println(" không chèn được phần tử vào mảng.");
+            System.out.println(" Nhap lai");
+            position = scanner.nextInt();
         }
+        for (int i = 0, k = 0; i < array.length; i++) {
+            if (i == position) {
+                array[i] = value;
+                continue;
+            }
+            array[i] = OldArray[k];
+            k++;
+        }
+        System.out.println("Mang sau khi them vao:");
+        for (int i = 0 ; i <array.length;i++){
+            System.out.printf(array[i]+"\t");
         }
     }
 }
-
