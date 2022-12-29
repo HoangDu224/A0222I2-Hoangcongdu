09f8b411451af7296c7da040a9bbca823d1cfad8
Hoangdu224 <duhoang224@gmail.com>
2022-12-29 20:02:18 +0700

    update ss10

--- module4/ss10/baitap/GioHang/src/main/java/com/example/demo/model/Cart.java
+++ module4/ss10/baitap/GioHang/src/main/java/com/example/demo/model/Cart.java
@@ -1,84 +1,74 @@
 package com.example.demo.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Cart {
-    List<Product> productList = new ArrayList<>();
-
-    public Cart(List<Product> productList) {
-        this.productList = productList;
-    }
+    Map<Product, Integer> productIntegerMap = new HashMap<>();
 
     public Cart() {
     }
 
+    public Cart(Map<Product, Integer> productIntegerMap) {
+        this.productIntegerMap = productIntegerMap;
+    }
+
+    public Map<Product, Integer> getProductIntegerMap() {
+        return productIntegerMap;
+    }
+
+    public void setProductIntegerMap(Map<Product, Integer> productIntegerMap) {
+        this.productIntegerMap = productIntegerMap;
+    }
+
     public boolean checkItem(Product product) {
-        for (Product product1 : productList) {
-            if (product.getId() == product1.getId()) {
-                return true;
+       for (Map.Entry<Product,Integer> entry:productIntegerMap.entrySet()){
+           if (entry.getKey().getId()==product.getId()){
+               return true;
+           }
+       }
+       return false;
+    }
+    private Map.Entry<Product, Integer> selectItemInCart(Product product){
+        for (Map.Entry<Product, Integer> entry : productIntegerMap.entrySet()) {
+            if(entry.getKey().getId()==(product.getId())){
+                return entry;
             }
         }
-        return false;
+        return null;
     }
 
     public void addProduct(Product product) {
-        productList.add(product);
+        if (!checkItem(product)){
+            productIntegerMap.put(product,1);
+        }else {
+            Map.Entry<Product,Integer> itemEntry = selectItemInCart(product);
+            int quantity = itemEntry.getValue()+1;
+            productIntegerMap.replace(itemEntry.getKey(),quantity);
+        }
     }
 
-    public List<Product> findAll() {
-        return productList;
-    }
-
+   
     public int countPrice() {
         int price = 0;
-        for (Product product : productList) {
-            price += product.getPrice();
+        for (Map.Entry<Product,Integer> entry:productIntegerMap.entrySet()) {
+            price += entry.getKey().getPrice()*entry.getValue();
         }
         return price;
     }
 
     public int countQuantity() {
-        return productList.size();
-    }
-
-    public int countProduct() {
-        List<Product> products = new ArrayList<>();
-        products.add(productList.get(0));
-        for (Product product : productList) {
-            for (int i = 0; i < products.size(); i++) {
-                if (product.getId() == products.get(i).getId()) {
-                    continue;
-                } else {
-                    products.add(product);
-                }
-            }
-        }
-        return products.size();
-    }
-
-    public int countProductQuantity(Product product) {
         int quantity = 0;
-        for (Product product1 : productList) {
-            if (product.getId() == product1.getId()) {
-                quantity++;
-            }
+        for (Map.Entry<Product,Integer> entry:productIntegerMap.entrySet()) {
+            quantity += entry.getValue();
         }
         return quantity;
     }
-    public List<Product> mergeProduct(){
-        List<Product> products = new ArrayList<>();
-        products.add(productList.get(0));
-        for (Product product : productList) {
-            for (int i = 0; i < products.size(); i++) {
-                if (product.getId() == products.get(i).getId()) {
-                    continue;
-                } else {
-                    products.add(product);
-                }
-            }
-        }
-        return products;
+
+    public int countProduct() {
+        return productIntegerMap.size();
     }
 
 }
