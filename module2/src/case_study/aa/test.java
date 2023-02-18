9a51d7c0bd706a685980160e09569bea6ce517a0
Hoangdu224 <duhoang224@gmail.com>
2023-02-19 06:25:46 +0700

    update ss02 ss03 module5

--- module2/src/case_study/aa/test.java
+++ module2/src/case_study/aa/test.java
@@ -1,9 +1,22 @@
 package case_study.aa;
 
-import case_study.contronller.FuramaController.DisplayMainMenu;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class test {
     public static void main(String[] args) {
-
+        for (int i = 1 ; i<10;i++){
+            System.out.println(findFibonacci(i));
+        }
+    }
+    public static int findFibonacci(int n){
+        if (n<0){
+            return -1;
+        }else if (n == 0 || n == 1 ){
+            return n;
+        }else {
+            return findFibonacci(n-1)+findFibonacci(n-2);
+        }
     }
 }
